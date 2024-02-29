b80a4370f9d80939c23926892e68e02fc13527c0
DimaL123 <litvinenkodimaimmm@gmail.com>
2024-02-29 11:05:37 +0200

    add_filter

--- films/filmsapp/urls.py
+++ films/filmsapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.FilmView.as_view()),
+    path("filter/", views.FilterFilmList.as_view(), name="filter"),
     #path("<slug:slug>/", views.FilmDetailView.as_view()),
     path("<slug:slug>/", views.FilmDetailView.as_view(),name="film_detail"),
     path("review/<int:pk>/", views.AddReviev.as_view(), name="add_review"),
--- films/filmsapp/views.py
+++ films/filmsapp/views.py
@@ -1,9 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
 
 from django.views.generic import ListView, DetailView
 
-from .models import Film,Category, Actor
+from .models import Film,Category, Actor, Genre
 from .forms import ReviewForm
 
 
@@ -17,15 +18,20 @@ from .forms import ReviewForm
 #     def get(self, request, slug):
 #         films = Film.objects.get(url=slug)
 #         return render(request, "films/film_detail.html", {"films": films})
+class GenreYear:
+    """Жанри і роки"""
+    def get_genres(self):
+        return Genre.objects.all()
+    def get_years(self):
+        return Film.objects.filter(draft=False).values("uear")
 
-
-class FilmView(ListView):
+class FilmView(GenreYear,ListView):
     model = Film
     queryset = Film.objects.filter(draft=False)
 
 
 
-class FilmDetailView(DetailView):
+class FilmDetailView(GenreYear,DetailView):
     model = Film
     slug_field = "url"
 
@@ -45,7 +51,19 @@ class AddReviev(View):
             form.save()
         return redirect(film.get_absolute_url())
 
-class ActorView(DetailView):
+class ActorView(GenreYear,DetailView):
     model = Actor
     template_name = 'filmsapp/actor.html'
     slug_field = "name"
+
+
+
+
+
+class FilterFilmList(GenreYear, ListView):
+    def get_queryset(self):
+        queryset = Film.objects.filter(
+            Q(uear__in=self.request.GET.getlist("uear"))|
+            Q(genre__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
